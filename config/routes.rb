5dc12be238c64bd621c134a18ab88630a8931e46
EC2 Default User <ec2-user@ip-172-31-7-102.ap-northeast-1.compute.internal>
2023-06-20 11:46:49 +0000

    Add Top

--- config/routes.rb
+++ config/routes.rb
@@ -46,8 +46,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
 
-    root 'homes#top'
-
+  root 'homes#top'
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
 
     resources :genres, only: [:index, :create, :edit, :update]
