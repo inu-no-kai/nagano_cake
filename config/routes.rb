c7a8959648a2e576692daa5a97ab64f801bf7c4b
KotaMurasugi <132252846+KotaMurasugi@users.noreply.github.com>
2023-06-20 09:44:42 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,63 @@
+Rails.application.routes.draw do
+
+  # 顧客用
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  namespace :public do
+
+    root 'homes#top'
+
+    resources :items, only: [:index, :show]
+
+    resources :customers, only: [:show, :edit, :update,]
+      resources :customers do
+        collection do
+          get 'quit'
+          patch 'unsubscribe'
+        end
+      end
+
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+      resources :cart_items do
+        collection do
+          delete 'destroy_all'
+        end
+      end
+
+    resources :orders, only: [:new, :index, :show, :create]
+      resources :orders do
+        collection do
+          post 'confirm'
+          get 'thanks'
+        end
+      end
+
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+
+  end
+
+  # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+
+    root 'homes#top'
+
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+
+    resources :genres, only: [:index, :create, :edit, :update]
+
+    resources :customers, only: [:index, :show, :edit, :update]
+
+    resources :orders, only: [:show, :update]
+
+    resources :order_details, only: [:update]
+
+  end
+
+end
---
+++ db/migrate/20230620093033_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+
+      t.string  :name,        null: false
+      t.text    :description, null: false
+      t.integer :price,       null: false
+      t.boolean :is_sold_out, null: false, default: false
+      t.timestamps
+    end
+  end
+end
