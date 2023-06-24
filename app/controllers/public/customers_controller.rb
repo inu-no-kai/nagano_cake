c2f940bd9d4f5645a68474f6ce9fff8420ced7dd
KotaMurasugi <132252846+KotaMurasugi@users.noreply.github.com>
2023-06-24 19:14:25 +0000

    add mypage

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,13 @@
 class Public::CustomersController < ApplicationController
+  def show
+    @customer = Customer.find(params[:id])
+  end
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+  private
+
+  def customer_params
+  	  params.require( :customer ).permit( :last_name, :first_name, :last_name_kana, :first_name_kana, :phone_number, :email, :password, :post_code, :address )
+  end
 end
