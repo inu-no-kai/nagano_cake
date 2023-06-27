d2c3b99a4cf2e56e1515d3b97c92b9980ac6ec75
EC2 Default User <ec2-user@ip-172-31-37-184.ap-northeast-1.compute.internal>
2023-06-27 10:20:55 +0000

    ページネーションの実装　管理者の会員の実装完了

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,29 @@
 class Admin::CustomersController < ApplicationController
+  
+    def index
+      @customers = Customer.page(params[:page]).per(10)
+    end
+    
+    def show
+      @customer = Customer.find(params[:id])
+    end
+    
+    def edit
+      @customer = Customer.find(params[:id])
+    end
+    
+    def update
+      @customer = Customer.find(params[:id])
+      if @customer.update(customer_params)
+        redirect_to admin_customer_path(@customer), notice: '会員情報を更新しました。'
+      else
+        render :edit
+      end
+    end
+    
+    private
+  
+    def customer_params
+      params.require(:customer).permit(:last_name, :first_name, :post_code, :address, :phone_number, :email, :is_delete)
+    end
 end
