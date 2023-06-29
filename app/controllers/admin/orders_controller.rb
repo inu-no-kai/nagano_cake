c554b2dd1e70431d4c5d1c1fc0f1bf771b6ed56e
KotaMurasugi <132252846+KotaMurasugi@users.noreply.github.com>
2023-06-29 09:17:17 +0000

    admin customer data complete

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,29 @@
 class Admin::OrdersController < ApplicationController
+
+  def show
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: @order.id)
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: @order.id)
+    @order.update(order_params)
+
+    if @order.order_status == "payment_confirmation"
+      @order_details.each do |order_detail|
+        order_detail.update(making_status: "producting")
+      end
+    end
+
+    redirect_to admin_order_path(@order.id)
+  end
+
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment_method, :customer_id, :delivery_post_code, :delivery_address, :delivery_name ,:postage ,:invoice_amount,:order_status)
+  end
+
 end
